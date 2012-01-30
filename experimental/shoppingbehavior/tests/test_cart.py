0476890546771ae791a0bc5738fa1404ee780952
Jesse Snyder <jsnyder@wesleyan.edu>
2012-01-29 19:34:10 -0800

    Clean up cart tests and add tests for cart view

--- experimental/shoppingbehavior/tests/test_cart.py
+++ experimental/shoppingbehavior/tests/test_cart.py
@@ -1,23 +1,64 @@
 import unittest2 as unittest
 import fudge
+from zope.interface import implements
+from zope.annotation.interfaces import IAttributeAnnotatable
 from experimental.shoppingbehavior import cart
+from zope.publisher.browser import TestRequest
 
 
-class TestCartUnit(unittest.TestCase):
+class SomeContext(object):
+    implements(IAttributeAnnotatable)
+
+
+class TestCart(unittest.TestCase):
 
     @fudge.patch('experimental.shoppingbehavior.cart.get_cart')
     @fudge.patch('experimental.shoppingbehavior.cart.queryMultiAdapter')
-    def testAdd(self, fake_get_cart, fake_queryMultiAdapter):
+    def testAdd(self, fake_get_cart, queryMultiAdapter):
         # set up a fake cart
-        (fake_get_cart.expects_call().returns_fake().provides('__setitem__'))
+        (fake_get_cart.expects_call()
+                      .returns_fake()
+                      .provides('__setitem__'))
         fixture = cart.Cart()
         # set up a fake queryMultiAdapter chain of madness
-        fakeLineItem = fudge.Fake('LineItem').has_attr(
-                                            item_id="foo", quantity=0)
-        fakeLineItemFactory = fudge.Fake('LineItemFactory').provides(
-                                            'create').returns(fakeLineItem)
-        (fake_queryMultiAdapter.expects_call().with_arg_count(2).
-                returns(fakeLineItemFactory))
+        fakeLineItem = (fudge.Fake('LineItem')
+                            .has_attr(item_id="foo", quantity=0))
+        fakeLineItemFactory = (fudge.Fake('LineItemFactory')
+                                    .provides('create')
+                                    .returns(fakeLineItem))
+        (queryMultiAdapter.expects_call()
+                          .with_arg_count(2)
+                          .returns(fakeLineItemFactory))
 
         self.assertEqual('foo', fixture.add(None, 3))
         self.assertEqual(3, fakeLineItem.quantity)
+
+
+class TestCartView(unittest.TestCase):
+    """docstring for TestCartView"""
+
+    @fudge.patch('experimental.shoppingbehavior.cart.Cart')
+    def testCartViewCallsWithZeroQtyByDefault(self, Cart):
+        context = SomeContext()
+        fakeCartInstance = (fudge.Fake()
+                                 .provides('add')
+                                 .with_args(context, 0)
+                                 .returns('some id'))
+        fakeCartInstance.expects('checkout')
+        Cart.expects_call().returns(fakeCartInstance)
+        view = cart.CartView(context, TestRequest())
+        view.update()
+
+    @fudge.patch('experimental.shoppingbehavior.cart.Cart')
+    def testCartViewCallsWithQtyFromRequest(self, Cart):
+        context = SomeContext()
+        testQty = '3'
+        request = TestRequest(form=dict(quantity=testQty))
+        fakeCartInstance = (fudge.Fake()
+                                 .provides('add')
+                                 .with_args(context, int(testQty))
+                                 .returns('some id'))
+        fakeCartInstance.expects('checkout')
+        Cart.expects_call().returns(fakeCartInstance)
+        view = cart.CartView(context, request)
+        view.update()
